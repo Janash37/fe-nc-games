51b28ef2c72b68dd1ea88af7126f2cc55035a96e
Janash37 <janash_37@hotmail.co.uk>
2022-07-25 15:37:17 +0100

    add the ability to view all game reviews

---
+++ src/components/AllReviews.jsx
@@ -0,0 +1,39 @@
+import { getAllReviews } from "../utils/api";
+import { useState, useEffect } from "react";
+
+export default function ReviewsHome() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    getAllReviews().then((reviews) => {
+      setReviews(reviews);
+      setIsLoading(false);
+    });
+  }, []);
+
+  return (
+    <section id="reviews-homepage">
+      {isLoading ? (
+        <p>Loading reviews..</p>
+      ) : (
+        <div>
+          <ul id="all-reviews-list">
+            {reviews.map((review) => {
+              return (
+                <li id="all-reviews-li" key={review.created_at}>
+                  <p>
+                    <strong>{review.title}</strong>
+                  </p>
+                  <p>By: {review.owner}</p>
+                  <p>Votes: {review.votes}</p>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      )}
+    </section>
+  );
+}
---
+++ src/components/ReviewsHome.jsx
@@ -0,0 +1,8 @@
+export default function ReviewsHome() {
+  return (
+    <section id="reviews-homepage">
+      <p>Reviews page</p>
+      {/* On load, run selectAllCategories and display them */}
+    </section>
+  );
+}
